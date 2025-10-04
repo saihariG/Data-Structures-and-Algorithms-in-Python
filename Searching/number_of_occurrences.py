d4e783f91bc46130f858cb4e7b6e188604263cf9
saihariG <krishnansaihari@gmail.com>
2025-10-04 12:48:45 -0400

    deleted pycache

--- Searching/number_of_occurrences.py
+++ Searching/number_of_occurrences.py
@@ -1,5 +1,40 @@
-from first_occurrence import first_occurrence
-from last_occurrence import last_occurrence
+def first_occurrence(arr, element):
+
+    low = 0
+    high = len(arr) - 1
+
+    while low < high:
+        mid = int((low + high)/2)
+
+        if element > arr[mid]:
+            low = mid + 1
+        else:
+            high = mid
+
+    if element == arr[low]:
+        return low
+    else:
+        return -1
+
+def last_occurrence(arr, element):
+
+    low = 0
+    high = len(arr) - 1
+
+    while low < high:
+        # The intuition here is you want to move the search towards the rightmost occurrence
+        # So, you need the midpoint to bias slightly towards the right, which justifies the +1.
+        mid = int((low + high + 1) / 2)
+
+        if element < arr[mid]:
+            high = mid-1
+        else:
+            low = mid
+
+    if element == arr[low]:
+        return low
+    else:
+        return -1
 
 def number_of_occurrences(arr, element):
     first = first_occurrence(arr, element)
