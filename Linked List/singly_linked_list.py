62d1c895a084493b2b02542a449d1e79d8a9e9e7
saihariG <krishnansaihari@gmail.com>
2025-10-04 13:11:16 -0400

    Singly Linked List Implementation

---
+++ Linked List/singly_linked_list.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    #  Algo to insert node at beginning of the list
+    #   1. create a new node and pass the data to be inserted
+    #   2. now make its next to point head
+    #   3. update head
+    def insert_at_first(self, data):
+        new_node = ListNode(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Algo to insert node at the end of the list
+    # 1. Create a new node
+    # 2. If the list is empty, insert node at the head and return
+    # 3. Create a current node pointing to the head and traverse until it's next is not null
+    # 4. Then, insert the new_node to current's next
+    def insert_at_last(self, data):
+        new_node = ListNode(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next is not None:
+            current = current.next
+
+        current.next = new_node
+
+
+    # Algorithm for counting length
+    #   1. have a counter and initialize to zero
+    #   2. create a new node current and initialize to head
+    #   3. Traverse till the end of the list
+    #   4. and increase the counter
+    def count_and_print_length(self):
+        n = 0
+
+        current = self.head
+        while current is not None:
+            current = current.next
+            n += 1
+
+        print(f"Length of linked list: {n}")
+
+
