cf33fc54bb36d54dba6b12bce20aaf3291becbcd
saihariG <krishnansaihari@gmail.com>
2025-10-12 18:15:30 -0400

    missed tail in dll

--- Linked List/doubly_linked_list.py
+++ Linked List/doubly_linked_list.py
@@ -7,17 +7,21 @@ class ListNode:
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     # Algo to insert node at beginning
     def insert_node_at_first(self, data):
         new_node = ListNode(data)
 
-        # if the list is not empty
-        if self.head is not None:
-            new_node.next = self.head
-            self.head.prev = new_node
-
         # if list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        # if the list is not empty
+        new_node.next = self.head
+        self.head.prev = new_node
         self.head = new_node
 
     # Algo to insert node at end
@@ -27,13 +31,10 @@ class DoublyLinkedList:
         # if the list is empty
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
             return
 
-        current = self.head
-
-        while current.next:
-            current = current.next
-
         # we reached the end of the list
-        current.next = new_node
-        new_node.prev = current
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
