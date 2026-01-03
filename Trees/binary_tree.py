a34606c75c6715259b6b3048916fe325326bda93
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-01-03 00:50:18 -0500

    binary tree setup

---
+++ Trees/binary_tree.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def create_binary_tree():
+    first = Node(1)
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+    fifth = Node(5)
+
+    root = first
+    root.left = second
+    root.right = third
+
+    second.left = fourth
+    second.right = fifth
+
+
+def create_binary_tree_2():
+    first = Node(1)
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+    fifth = Node(5)
+    sixth = Node(6)
+    seven = Node(7)
+
+    root = first
+    root.left = second
+    root.right = third
+
+    second.left = fourth
+    second.right = fifth
+
+    third.left = sixth
+    third.right = seven
+
+class binaryTree:
+    def recursive_inorder_traversal(self, root):
+        pass
+
+    def iterative_inorder_traversal(self, root):
+        pass 
+
+    def recursive_preorder_traversal(self, root):
+        pass
+
+    def iterative_preorder_traversal(self, root):
+        pass
+
+    def recursive_postorder_traversal(self, root):
+        pass
+
+    def iterative_postorder_traversal(self, root):
+        pass 
+
+    def level_order_traversal(self, root):
+        pass 
+
+    def height_of_binary_tree(self, root):
+        pass
+
+if __name__ == '__main__':
+    bt = binaryTree() 
+
+    create_binary_tree()
